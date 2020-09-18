e90e4dad39e5d5d57ed44a5ca03ca066dcb59490
Palamore <mlka444@naver.com>
2020-09-18 12:24:49 +0900

    Missile, Laser Turret Upload

---
+++ Turret_Laser.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret_Laser : Turret_Parent
+{ 
+
+    [Header("Laser Turret Fields")]
+    public Transform FirePoint;
+    public LineRenderer lineRenderer;
+    public ParticleSystem impactEffect;
+    public Light impactLight;
+    public float debuffSpeed;
+
+
+    protected override void UpgradeConfirm()
+    {
+        baseDamage = baseDamage * USM.GetDmgRate()[3];
+        damage = baseDamage;
+    }
+
+    protected override void Fire()
+    {
+        Laser();
+    }
+
+    protected override void StopFire()
+    {
+        if (lineRenderer.enabled)
+            lineRenderer.enabled = false;
+            impactEffect.Stop();
+            impactLight.enabled = false;
+
+    }
+
+    private void Laser()
+    {
+        if (!lineRenderer.enabled)
+        {
+            lineRenderer.enabled = true;
+            impactEffect.Play();
+            impactLight.enabled = true;
+        }
+
+        lineRenderer.SetPosition(0, FirePoint.position);
+        lineRenderer.SetPosition(1, target.position);
+
+        Vector3 dir = FirePoint.position - target.position;
+
+        impactEffect.transform.position = target.position + dir.normalized * 0.5f;
+        impactEffect.transform.rotation = Quaternion.LookRotation(dir);
+
+        Enemy e = target.GetComponent<Enemy>();
+        if(e!= null)
+        {
+            e.TakeDamage(damage / (60.0f / fireRate));
+            e.debuff(debuffSpeed);
+        }
+    }
+    
+}
---
+++ Turret_Missile.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret_Missile : Turret_Parent
+{
+    [Header("Missile Turret Fields")]
+    public Transform[] FirePoints = new Transform[8];
+    public GameObject MissilePrefab;
+    private float fireCountdown = 0.0f;
+
+
+    protected override void UpgradeConfirm()
+    {
+        baseDamage = baseDamage * USM.GetDmgRate()[7];
+        damage = baseDamage;
+        baseFireRate = baseFireRate * USM.GetAtkSpdRate()[7];
+        fireRate = baseFireRate;
+    }
+
+    protected override void Fire()
+    {
+        if (fireCountdown <= 0f)
+        {
+            Launch();
+            fireCountdown = 1.5f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    protected override void StopFire()
+    {
+
+    }
+
+    private void Launch()
+    {
+        GameObject[] missileGO = new GameObject[FirePoints.Length];
+        for (int i = 0; i < FirePoints.Length; i++)
+        {
+            missileGO[i] = (GameObject)Instantiate(MissilePrefab, FirePoints[i].position, FirePoints[i].rotation);
+            Missile missile = missileGO[i].GetComponent<Missile>();
+            if (missile != null)
+            {
+                missile.Seek(target);
+                missile.Set_Damage(damage);
+                if (bIsLV5)
+                    missile.explosionRadius = 10.0f;
+            }
+        }
+    }
+
+}
